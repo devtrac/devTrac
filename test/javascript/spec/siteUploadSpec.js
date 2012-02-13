685ebad68a04ef0550799f75d3a4bcff2b6ba057
Lei Shengtao <stlei@thoughtworks.com>
2012-02-13 18:11:09 +0800

    [stlei&xfwang] #4 Trimed ending-line spaces.

--- test/javascript/spec/siteUploadSpec.js
+++ test/javascript/spec/siteUploadSpec.js
@@ -4,11 +4,11 @@ describe("site.upload" ,function(){
 		var fixture = " <div id='site_list' class='grid'>" +
     	"<div class='grid_row'>" +
     	"<input type='checkbox' id='1_upload' >site1</input>" +
-			"<input type='checkbox' id='2_upload' >site2</input>" +  	
+			"<input type='checkbox' id='2_upload' >site2</input>" +
     	"</div>"+
 		"</div>";
 		setFixtures(fixture);
-		 
+
 		siteUpload = new SiteUpload();
 	});
 
@@ -16,20 +16,20 @@ describe("site.upload" ,function(){
 		it("update site's upload attr according to the checkbox's status",function(){
 			var sites = [{id:1,upload:false}, {id:2,upload:false}];
 			$("#1_upload").attr("checked",true);
-			
+
 			siteUpload.prepare(sites);
-			
+
 			expect(sites[0].upload).toEqual(true);
 		 	expect(sites[1].upload).toEqual(false);
 		});
 	});
-	
+
 	describe("unchecked", function(){
 		it("return false if the site is checked", function(){
 			var sites = [{id:1,upload:false}, {id:2,upload:false}]
 			$("#1_upload").attr("checked",true);
 			siteUpload.prepare(sites);
-			
+
 			expect(siteUpload.unchecked(sites[0])).toEqual(false);
 		})
 
@@ -37,7 +37,7 @@ describe("site.upload" ,function(){
 			var sites = [{id:1,upload:false}, {id:2,upload:false}];
 			$("#1_upload").attr("checked",true);
 			siteUpload.prepare(sites);
-			
+
 			expect(siteUpload.unchecked(sites[1])).toEqual(true);
 		})
 	})
