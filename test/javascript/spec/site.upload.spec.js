b8d0a7607ab640a2ebe8560570a08b000c2e13e1
Li Gao <lgao@thoughtworks.com>
2012-02-17 11:50:56 +0800

    [rrliu&xfwang] #4 Finish testing all uploading successfully with fake data

--- test/javascript/spec/site.upload.spec.js
+++ test/javascript/spec/site.upload.spec.js
@@ -3,28 +3,32 @@ describe("SiteUpload", function(){
 	describe("upload all sites successfully", function(){
 
 		var uploader = new SiteUpload();
-		var sites = [1, 2, 3];
 		var progressCallback = jasmine.createSpy('uploader.progressCallback');
 		var successCallback = jasmine.createSpy('uploader.successCallback');
 		var errorCallback = jasmine.createSpy('uploader.errorCallback');
-		navigator.network = {XHR: jasmine.createSpy('navigator.network.XHR')};
 
-		uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
+		beforeEach(function(){
+			var sites = [1, 2, 3];
+			spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
+				successCallback({'#data':'data_string'});
+			})
+			uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
+		})
 
-		it("progress callback should be called at right times", function(){
+		it("should call progress callback for right times", function(){
 			expect(progressCallback.callCount).toEqual(3);
 		});
 		
-		it("success callback should be called", function(){
+		it("should call success callback", function(){
 			expect(successCallback).toHaveBeenCalled();
 		});
 
-		it("error callback should not be called", function(){
+		it("should NOT call error callback", function(){
 			expect(errorCallback).not.toHaveBeenCalled();
 		});
 		
 		// TODO verify the arguments especially for the data to be sent
-		it("navigator.network.XHR should be called at right times", function(){
+		it("should call navigator.network.XHR for right times", function(){
 			expect(navigator.network.XHR.callCount).toEqual(3);
 		});
 	});
