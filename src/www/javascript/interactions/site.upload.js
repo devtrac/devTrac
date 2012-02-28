958b71ce7f42c2918a6780c876bb08678f590d55
Li Gao <lgao@thoughtworks.com>
2012-02-28 16:40:28 +0800

    [rrliu&xfwang] #4 Removed unused parameter: uploadedSites

--- src/www/javascript/interactions/site.upload.js
+++ src/www/javascript/interactions/site.upload.js
@@ -2,7 +2,7 @@ function SiteUpload(){
 }
 
 SiteUpload.prototype.uploadMultiple = function(sites, progressCallback, successCallback, errorCallback){
-	devtrac.siteUpload._uploadInternal(sites, {}, progressCallback, successCallback, errorCallback);
+	devtrac.siteUpload._uploadInternal(sites, progressCallback, successCallback, errorCallback);
 }
 
 SiteUpload.prototype.upload = function(site, successCallback, errorCallback){
@@ -45,12 +45,12 @@ SiteUpload.prototype.upload = function(site, successCallback, errorCallback){
     });
 }
 
-SiteUpload.prototype._uploadInternal = function(sites, uploadedSites, progressCallback, successCallback, errorCallback){
+SiteUpload.prototype._uploadInternal = function(sites, progressCallback, successCallback, errorCallback){
 	var siteToUpload = sites.pop();
 	if (siteToUpload) {
 		devtrac.siteUpload.upload(siteToUpload, function(msg) {
 			progressCallback(msg);
-			devtrac.siteUpload._uploadInternal(sites, uploadedSites, progressCallback, successCallback, errorCallback);
+			devtrac.siteUpload._uploadInternal(sites, progressCallback, successCallback, errorCallback);
 		}, function(err) {
 			progressCallback(err);
 			errorCallback(err);
