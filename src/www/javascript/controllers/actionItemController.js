82fe1c70de903bfe4814ab74f0d6a05c4756482b
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-02-28 16:20:27 +0800

    [lgao&stlei] Bug fix - fix the assign to item N/A problem in action item screen show

--- src/www/javascript/controllers/actionItemController.js
+++ src/www/javascript/controllers/actionItemController.js
@@ -1,31 +1,27 @@
 function ActionItemController(){
-	var currentActionItem;
+    var currentActionItem;
 }
 
 ActionItemController.prototype.edit = function(actionItem){
-	currentActionItem = actionItem;
+    currentActionItem = actionItem;
     navigator.log.debug("Editing action item");
     screens.show("loading");
     $("#action_item_title_edit").val(actionItem.title);
     $("#action_item_task_edit").val(actionItem.task);
-	var users = $("#action_item_assigned_to_edit");
+    var users = $("#action_item_assigned_to_edit");
     users.html("");
     $(devtrac.profiles).each(function(index, profile){
-        users.append("<option value='" + profile.username + "'>" + profile.name + "</option>");
+        users.append("<option value='" + profile.uid + "'>" + profile.name + "</option>");
     });
-
-	var profiles = $.grep(devtrac.profiles, function(profile){
-			return actionItem.assignedTo == profile.username;
-        });
-	var name = profiles.length > 0 ? profiles[0].name : "N/A";
-	$("#action_item_assigned_to_edit option[text=" + name+ "]").attr("selected","selected");
+    var name = devtrac.actionItemController._parseProfileName(actionItem);
+    $("#action_item_assigned_to_edit option[text=" + name + "]").attr("selected", "selected");
 
     screens.show("action_item_edit");
     navigator.log.debug("Displayed action item screen");
 }
 
 ActionItemController.prototype.editSave = function(){
-	navigator.log.debug("Saving action item");
+    navigator.log.debug("Saving action item");
     var title = $("#action_item_title_edit").val();
     var task = $("#action_item_task_edit").val();
     var assignedTo = $("#action_item_assigned_to_edit").val();
@@ -35,86 +31,84 @@ ActionItemController.prototype.editSave = function(){
         return;
     }
 
-	for(var id in devtrac.currentSite.actionItems){
-		var actionItem = devtrac.currentSite.actionItems[id];
-		if(currentActionItem.id == actionItem.id){
-			devtrac.currentSite.actionItems[id].title = title;
-			devtrac.currentSite.actionItems[id].task = task;
-			devtrac.currentSite.actionItems[id].assignedTo= assignedTo;
-		}
-	}
+    for (var id in devtrac.currentSite.actionItems) {
+        var actionItem = devtrac.currentSite.actionItems[id];
+        if (currentActionItem.id == actionItem.id) {
+            devtrac.currentSite.actionItems[id].title = title;
+            devtrac.currentSite.actionItems[id].task = task;
+            devtrac.currentSite.actionItems[id].assignedTo = assignedTo;
+        }
+    }
 
     devtrac.dataStore.saveCurrentSite(function(){
         alert("Edited action item.");
         navigator.log.debug("Edited action item. Will display list.");
-		devtrac.actionItemController.show();
+        devtrac.actionItemController.show();
     });
 }
 
 ActionItemController.prototype.show = function(){
     navigator.log.debug("Showing action items");
-	screens.show("loading");
+    screens.show("loading");
     var actionItemGrid = $(".action_item_grid");
 	
-	var fakeItems = [];
-	var fakeActionItem = new ActionItem();
-		fakeActionItem.id = 1;
-		fakeActionItem.title = "fake 1";
-		fakeActionItem.task = "test fake";
-		fakeActionItem.assignedTo = "Me";
-	fakeItems.push(fakeActionItem);
-	if (devtrac.currentSite.actionItems.length == 0 && fakeItems.length == 0) {
+    var fakeItems = [];
+    var fakeActionItem = new ActionItem();
+    fakeActionItem.id = 1;
+    fakeActionItem.title = "fake 1";
+    fakeActionItem.task = "test fake";
+    fakeActionItem.assignedTo = "Me";
+    fakeItems.push(fakeActionItem);
+    if (devtrac.currentSite.actionItems.length == 0 && fakeItems.length == 0) {
         $("#no_action_items").show();
         actionItemGrid.hide();
         screens.show("list_action_items");
         return;
     }
 
-    var container = $("#action_items_list");
-	var previousContainer = $("#previous_action_items_list");
     $("#no_action_items").hide();
 
-	devtrac.actionItemController.displayActionItemInCurrentSection(devtrac.currentSite.actionItems);
-	devtrac.actionItemController.displayActionItemInHistorySection(fakeItems);
-	actionItemGrid.show();
+    devtrac.actionItemController.displayActionItemInCurrentSection(devtrac.currentSite.actionItems);
+    devtrac.actionItemController.displayActionItemInHistorySection(fakeItems);
+    actionItemGrid.show();
 
-	navigator.log.debug("Displayed action items");
+    navigator.log.debug("Displayed action items");
     screens.show("list_action_items");
-	attachClickEvents(".action_item", showActionItemEditScreen);
+    attachClickEvents(".action_item", showActionItemEditScreen);
 }
 
 ActionItemController.prototype.displayActionItemInCurrentSection = function(actionItems){
-	var container = $("#action_items_list");
-	container.html("");
-	$.each(actionItems, function(index, item){
-		var name =devtrac.actionItemController._parseProfileName(item);
-		var id = item.id;
-		var	html = "<div class='grid_row'><div id='" + id + "' class='col1 action_item link'>" + item.title + "</div><div class='col2'>" + name + "</div></div>";
+    var container = $("#action_items_list");
+    container.html("");
+    $.each(actionItems, function(index, item){
+        var name = devtrac.actionItemController._parseProfileName(item);
+        var id = item.id;
+        var html = "<div class='grid_row'><div id='" + id + "' class='col1 action_item link'>" + item.title + "</div><div class='col2'>" + name + "</div></div>";
         container.append(html);
     });
 }
 
 ActionItemController.prototype.displayActionItemInHistorySection = function(actionItems){
-	var container = $("#previous_action_items_list");
-	container.html("");
-	$.each(actionItems, function(index, item){
-		var name =devtrac.actionItemController._parseProfileName(item);
-		var id = item.id;
+    var container = $("#previous_action_items_list");
+    container.html("");
+    $.each(actionItems, function(index, item){
+        var name = devtrac.actionItemController._parseProfileName(item);
+        var id = item.id;
         var html = "<div class='grid_row'><div id='" + id + "' class='col1'>" + item.title + "</div><div class='col2'>" + name + "</div></div>";
-		container.append(html);
+        container.append(html);
     });
 }
 
-ActionItemController.prototype._parseProfileName= function(actionItem){
-	var profiles = $.grep(devtrac.profiles, function(profile){
-			return actionItem.assignedTo == profile.username;
-        });
-	return profiles.length > 0 ? profiles[0].name : "N/A";
+ActionItemController.prototype._parseProfileName = function(actionItem){
+    var profiles = $.grep(devtrac.profiles, function(profile){
+        return actionItem.assignedTo == profile.uid || actionItem.assignedTo == profile.username;
+    });
+    return profiles.length > 0 ? profiles[0].name : "N/A";
 }
 
 ActionItemController.prototype.add = function(){
     navigator.log.debug("Adding action item");
-	$("#action_item_title").val("");
+    $("#action_item_title").val("");
     $("#action_item_task").val("");
     var users = $("#action_item_assigned_to");
     users.html("");
@@ -122,11 +116,11 @@ ActionItemController.prototype.add = function(){
         users.append("<option value='" + profile.username + "'>" + profile.name + "</option>");
     });
     screens.show("add_action_item");
-	navigator.log.debug("Displayed add action item screen");
+    navigator.log.debug("Displayed add action item screen");
 }
 
 ActionItemController.prototype.save = function(){
-	navigator.log.debug("Saving action item");
+    navigator.log.debug("Saving action item");
     var title = $("#action_item_title").val();
     var task = $("#action_item_task").val();
     var assignedTo = $("#action_item_assigned_to").val();
@@ -137,7 +131,7 @@ ActionItemController.prototype.save = function(){
     }
 
     var actionItem = new ActionItem();
-	actionItem.id = 0;
+    actionItem.id = 0;
     actionItem.title = title;
     actionItem.task = task;
     actionItem.assignedTo = assignedTo;
@@ -147,6 +141,6 @@ ActionItemController.prototype.save = function(){
     devtrac.dataStore.saveCurrentSite(function(){
         alert("Added action item.");
         navigator.log.debug("Saved action item. Will display list.");
-		devtrac.actionItemController.show();
+        devtrac.actionItemController.show();
     });
 }
