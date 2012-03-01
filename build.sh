4d56353a7565e05c13fd77604efb581cd70e5658
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-03-01 14:47:45 +0800

    [stlei&xfwang] Be able to compile and run JUnit test

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ mkdir -p deliverables/Standard/4.6.0/
 ant -buildfile build.xml
 
 # Package
-java -jar build/dependency/rapc.jar -convertpng codename=deliverables/Standard/4.6.0/devTracBB -sourceroot=src -import=lib/net_rim_api.jar build/dependency/devTracBB.rapc bin
+java -jar build/dependency/rapc.jar -convertpng codename=deliverables/Standard/4.6.0/devTracBB -sourceroot=src/main -import=lib/net_rim_api.jar build/dependency/devTracBB.rapc bin/main
 
 # Sign
 java -jar build/dependency/SignatureTool.jar -a -C -p r0ys1ngh4m deliverables/Standard/4.6.0/devTracBB.cod
