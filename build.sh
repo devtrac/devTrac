018945d06595b77fd684e5e0e6456a5a97e3e26b
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-03-02 18:25:22 +0800

    [xfwang&stlei] Added package, sign, load ant targets and update build.sh correspondingly.

--- build.sh
+++ build.sh
@@ -2,22 +2,17 @@
 
 set -e
 
+ant_log="ant.log.$$"
+
 # Build
-ant -buildfile build.xml
+# ant -buildfile build.xml sign | tee "$ant_log"
 
-# Package
-package_log="/tmp/package.log.$$"
+# Check package result
+cat "$ant_log" | grep 'No errors.' &> /dev/null
 
-java -jar build/dependency/rapc.jar -convertpng codename=deliverables/Standard/4.6.0/devTracBB -sourceroot=src/main -import=lib/net_rim_api.jar build/dependency/devTracBB.rapc bin/main | tee "$package_log"
-tail -n 1 "$package_log" | grep 'No errors.' &> /dev/null 
+# Check sign result
+cat "$ant_log" | grep 'Success.' &> /dev/null
 
-rm "$package_log"
-
-# Sign
-sign_log="/tmp/sign.log.$$"
-
-java -jar build/dependency/SignatureTool.jar -a -C -p r0ys1ngh4m deliverables/Standard/4.6.0/devTracBB.cod | tee "$sign_log"
-tail -n 1 "$sign_log" | grep 'Success.' &> /dev/null 
-
-rm "$sign_log"
+# Clean
+rm "$ant_log"
 
