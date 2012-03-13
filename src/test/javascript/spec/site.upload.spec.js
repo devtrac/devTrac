4e138c57f0987babbd7e3a4fa1b68e7170ee36b7
Li Gao <lgao@thoughtworks.com>
2012-03-13 16:49:39 +0800

    [rrliu&xfwang] #9 Format: retab

--- src/test/javascript/spec/site.upload.spec.js
+++ src/test/javascript/spec/site.upload.spec.js
@@ -1,92 +1,97 @@
 describe("SiteUpload", function(){
 
-	var uploader = new SiteUpload();
-	var progressCallback = jasmine.createSpy('uploader.progressCallback');
-	var successCallback = jasmine.createSpy('uploader.successCallback');
-	var errorCallback = jasmine.createSpy('uploader.errorCallback');
+    var sites;
+    var uploader = new SiteUpload();
+    var progressCallback;
+    var successCallback;
+    var errorCallback;
 
-	describe("upload all sites successfully", function(){
-		beforeEach(function(){
-			var sites = [1, 2, 3];
-			spyOn(devtrac.siteUpload, '_packageSite').andCallFake(function(site){
-				return [{'name':site}];
-			})
-			spyOn(devtrac.dataPush, 'serviceSyncSaveNode').andCallThrough();
-			spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
-				successCallback({'#data':'data_string'});
-			})
+    describe("upload all sites successfully", function(){
+        beforeEach(function(){
+            sites = [1, 2, 3];
+            progressCallback = jasmine.createSpy('uploader.progressCallback');
+            successCallback = jasmine.createSpy('uploader.successCallback');
+            errorCallback = jasmine.createSpy('uploader.errorCallback');
 
-			uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
-		})
+            spyOn(devtrac.siteUpload, '_packageSite').andCallFake(function(site){
+                return [{'name':site}];
+            })
+            spyOn(devtrac.dataPush, 'serviceSyncSaveNode').andCallThrough();
+            spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
+                successCallback({'#data':'data_string'});
+            })
 
-		it("should call progress callback for right times", function(){
-			expect(progressCallback.callCount).toEqual(6);
-		});
+            uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
+        })
 
-		it("should call success callback", function(){
-			expect(successCallback).toHaveBeenCalled();
-		});
+        it("should call progress callback for right times", function(){
+            expect(progressCallback.callCount).toEqual(6);
+        });
 
-		it("should NOT call error callback", function(){
-			expect(errorCallback).not.toHaveBeenCalled();
-		});
+        it("should call success callback", function(){
+            expect(successCallback).toHaveBeenCalled();
+        });
 
-		// TODO verify the arguments especially for the data to be sent
-		it("should call navigator.network.XHR for right times", function(){
-			expect(navigator.network.XHR.callCount).toEqual(3);
-		});
+        it("should NOT call error callback", function(){
+            expect(errorCallback).not.toHaveBeenCalled();
+        });
 
-		it("should call createBBSync with correct node data", function(){
-			expect(devtrac.dataPush.serviceSyncSaveNode.mostRecentCall.args).toEqual([[{'name':1}]]);
-		})
-	});
+        it("should call navigator.network.XHR for right times", function(){
+            expect(navigator.network.XHR.callCount).toEqual(3);
+        });
 
-	describe("should only upload the site which has been modified", function(){
+        it("should call createBBSync with correct node data", function(){
+            expect(devtrac.dataPush.serviceSyncSaveNode.mostRecentCall.args).toEqual([[{'name':3}]]);
+        })
+    });
 
-		beforeEach(function(){
+    describe("should only upload the site which has been modified", function(){
 
-			spyOn(devtrac.siteUpload, '_packageSite').andCallFake(function(site){
-				return [{'name':site}];
-			})
+        beforeEach(function(){
 
-			spyOn(devtrac.dataPush, 'serviceSyncSaveNode').andCallThrough();
+            spyOn(devtrac.siteUpload, '_packageSite').andCallFake(function(site){
+                return [{'name':site}];
+            })
 
-		})
+            spyOn(devtrac.dataPush, 'serviceSyncSaveNode').andCallThrough();
 
-		it("should only upload the site which uploaded is false", function(){
-			var sites = [{'name':1,'uploaded':false},{'name':2,'uploaded':true},{'name':3,'uploaded':false} ];
+        })
 
-			spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
-				successCallback({'#data':'data_string'});
-			})
+        it("should only upload the site which uploaded is false", function(){
+            sites = [{'name':1,'uploaded':false},{'name':2,'uploaded':true},{'name':3,'uploaded':false} ];
 
-			uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
+            spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
+                successCallback({'#data':'data_string'});
+            })
 
-			expect(navigator.network.XHR.callCount).toEqual(2);
-		})
+            uploader.uploadMultiple(sites, progressCallback, successCallback, errorCallback);
 
-		it("should update the uploaded status to true if uploading is succeeded", function(){
-			var site ={'name':1,'uploaded':false};
+            expect(navigator.network.XHR.callCount).toEqual(2);
+        })
 
-			spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
-				successCallback({'#data':'data_string'});
-			})
+        it("should update the uploaded status to true if uploading is succeeded", function(){
+            var site ={'name':1,'uploaded':false};
 
-			uploader.upload(site, successCallback, errorCallback)
+            spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
+                successCallback({'#data':'data_string'});
+            })
 
-			expect(site.uploaded).toEqual(true);
-		})
+            uploader.upload(site, successCallback, errorCallback)
 
-		it("should update the uploaded status to false if uploading is failed", function(){
-			var site ={'name':1,'uploaded':false};
+            expect(site.uploaded).toEqual(true);
+        })
 
-			spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
-				errorCallback({'#data':'data_string'});
-			})
+        it("should update the uploaded status to false if uploading is failed", function(){
+            var site ={'name':1,'uploaded':false};
 
-			uploader.upload(site, successCallback, errorCallback)
+            spyOn(navigator.network, 'XHR').andCallFake(function(URL, POSTdata, successCallback, errorCallback){
+                errorCallback({'#data':'data_string'});
+            })
 
-			expect(site.uploaded).toEqual(false);
-		})
-	})
-});
+            uploader.upload(site, successCallback, errorCallback)
+
+            expect(site.uploaded).toEqual(false);
+        })
+    })
+})
+
