20c0c9169ce0b19820ffb55541de863225462fc2
DevTrac-1 <xfwang@thoughtworks.com>
2012-04-11 10:22:57 +0800

    [rrliu] #16 Fix: if not containing GPS, show empty in displaying

--- src/main/www/javascript/controllers/gpsController.js
+++ src/main/www/javascript/controllers/gpsController.js
@@ -4,8 +4,17 @@ function GPSController(){
 
 GPSController.prototype.show = function(){
     var gpsInfo = devtrac.currentSite.placeGeo.substring(devtrac.currentSite.placeGeo.lastIndexOf("(")+1, devtrac.currentSite.placeGeo.lastIndexOf(")"));
-    var latitude = gpsInfo.split(" ")[0];
-    var longitude = gpsInfo.split(" ")[1];
+    var latitude;
+    var longitude;
+    if (gpsInfo == ""){
+        latitude = "";
+        longitude = "";
+    }
+    else{
+        latitude = gpsInfo.split(" ")[0];
+        longitude = gpsInfo.split(" ")[1];
+    }
+
     $("#latitude_edit").text("Latitude: " + latitude);
     $("#longitude_edit").text("Longitude: " + longitude);
     screens.show("gps_edit");
