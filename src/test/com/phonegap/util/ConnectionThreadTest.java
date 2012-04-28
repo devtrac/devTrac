2797f130154dc5b72eb8ed87ff9648af25dd8246
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-04-28 10:23:36 +0800

    [lgao&mmbai]-#14-retab

--- src/test/com/phonegap/util/ConnectionThreadTest.java
+++ src/test/com/phonegap/util/ConnectionThreadTest.java
@@ -1,32 +1,28 @@
 package com.phonegap.util;
 
-import com.phonegap.PhoneGap;
-import com.phonegap.util.ConnectionThread;
-
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class ConnectionThreadTest extends TestCase {
-	final String cookie = "SESS4df45f21b05409b30c5eca0efe59f22a=91tphJbS7weIiGaMhbk1CyBAiqumHzIw1oyyNmoHMno";
-	final String header = cookie + "; expires=Sun, 15-Apr-2012 09:57:54 GMT; path=/; domain=.geo.devtrac.org; HttpOnly";
-	
-	ConnectionThread connectionThread;
-	
-	public void setUp() {
-		connectionThread = new ConnectionThread(null);
-	}
+    final String cookie = "SESS4df45f21b05409b30c5eca0efe59f22a=91tphJbS7weIiGaMhbk1CyBAiqumHzIw1oyyNmoHMno";
+    final String header = cookie + "; expires=Sun, 15-Apr-2012 09:57:54 GMT; path=/; domain=.geo.devtrac.org; HttpOnly";
 
-	public void test_cookie_should_be_updated() {
-		connectionThread.updateCookie(header);
+    ConnectionThread connectionThread;
 
-		assertEquals(cookie, connectionThread.getCookie());
-	}
+    public void setUp() {
+        connectionThread = new ConnectionThread(null);
+    }
 
-	public void test_cookie_should_NOT_be_updated_when_cookie_does_not_exist() {
-		connectionThread.updateCookie(header);
+    public void test_cookie_should_be_updated() {
+        connectionThread.updateCookie(header);
 
-		connectionThread.updateCookie(null);
+        assertEquals(cookie, connectionThread.getCookie());
+    }
 
-		assertEquals(cookie, connectionThread.getCookie());
-	}
+    public void test_cookie_should_NOT_be_updated_when_cookie_does_not_exist() {
+        connectionThread.updateCookie(header);
+
+        connectionThread.updateCookie(null);
+
+        assertEquals(cookie, connectionThread.getCookie());
+    }
 }
